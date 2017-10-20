397a8b3215f72435d78792a2bb676be837f1235a
Dagmar <margretl16@ru.is>
2017-10-20 17:45:01 +0000

    Set up folder structure, build.gradle and made the java files

---
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -0,0 +1,15 @@
+package is.ru.TicTacToe;
+
+public class TicTacToe {
+
+	//here is where the code goes
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to tic tac toe!");
+
+
+	}
+
+}
+
---
+++ src/test/java/is/ru/TicTacToe/TicTacToeTests.java
@@ -0,0 +1,11 @@
+package is.ru.TicTacToe;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class TicTacToeTests {
+
+	//Here is where the unit tests go
+
+}
