393a49c1dd1e619bcdc13cf9d8a3fc3978744c2f
ValgardurRI <valgardur16@ru.is>
2017-10-24 18:07:42 +0000

    made a rough version of UI for tictactoe game

--- src/main/java/is/ru/TicTacToe/TicTacToeWebInterface.java
+++ src/main/java/is/ru/TicTacToe/TicTacToeWebInterface.java
@@ -7,11 +7,49 @@ import spark.utils.ResourceUtils;
 public class TicTacToeWebInterface {
 	public static void main(String[] args) {
 		TicTacToe ttt = new TicTacToe();
+		byte player = 1;
 		staticFileLocation("/public");
-		//get("/", (req, res) -> renderContent("index.html"));
-		get("/0", (req, res) -> "index.html");
+		get("/", (req, res) -> {
+			String output = "<!DOCTYPE html 5> " +
+							"<html> " +
+								"<head> " +
+								  "<link rel=\"stylesheet\" type=\"text/css\" href=\"/style/style.css\"> " +
+								"</head> " +
+								"<body> " +
+									"<table> ";
+			for(int i = 0; i<3; i++) {
+				output += "<tr> ";
+				for(int j = 0; j<3; j++) {
+					output += "<td> <a href=/"  + (i*3 + j) + "> ";
+					if(ttt.getValueAtPos(i*3 + j) == 0) {
+						output += "<img src=\"/images/blank.png\"> ";
+					}
+					else if(ttt.getValueAtPos(i*3 + j) == 1) {
+						output += "<img src=\"/images/cross.png\"> ";
+					}
+					else if(ttt.getValueAtPos(i*3 + j) == 2) {
+						output += "<img src=\"/images/circle.png\"> ";
+					}
+					
+					output += "</a> </td>";
+				}
+				output += "</tr> ";
+			}
+			output += 		"</table> " +
+						"</body> " +
+					"</html> ";
+			return output;
+		});
+		before("/:id", (req, res) -> {
+			byte playerRef = player;
+			if(playerRef == 1) {
+				ttt.insertX(Integer.parseInt(req.params(":id")));
+				playerRef = 2;
+			}else{
+				ttt.insertO(Integer.parseInt(req.params(":id")));
+				playerRef = 2;
+			}
+			res.redirect("/");
+		});
 	}
-	
-	//Taken from 
-	//https://stackoverflow.com/questions/33769455/java-spark-framework-use-straight-html-template
 }
