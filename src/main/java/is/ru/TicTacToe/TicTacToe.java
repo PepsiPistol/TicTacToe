37af32131e77c1fb2a500ae471bfe4b117822ef1
ValgardurRI <valgardur16@ru.is>
2017-10-24 19:27:04 +0000

    created tests for- and rewrote tictactoe.java to have only one insert and manage the order of which player inserts automatically. Rewrote old tests to have only one insert.

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -3,26 +3,20 @@ package is.ru.TicTacToe;
 public class TicTacToe {
 	
 	byte[] grid;
+	int player;
 	public TicTacToe() {
 		grid = new byte[9];
-
+		player = 0;
 		for(int i = 0; i<grid.length; i++) {
 			grid[i] = 0;	
 		}
 	}
 
-	public void insertX(int pos) {
+	public void insert(int pos) {
 		if(validatePos(pos)) {
 			if(grid[pos] == 0) {
-				grid[pos] = 1;
-			}
-		}
-	}
-
-	public void insertO(int pos) {
-		if(validatePos(pos)) {
-			if(grid[pos] == 0) {
-				grid[pos] = 2;
+				grid[pos] = (byte)(player + 1);
+				player = (byte)(player + 1)%2;
 			}
 		}
 	}
--- src/test/java/is/ru/TicTacToe/TicTacToeTests.java
+++ src/test/java/is/ru/TicTacToe/TicTacToeTests.java
@@ -7,18 +7,18 @@ import org.junit.Test;
 public class TicTacToeTests {
 
 	@Test
-	public void testXOnPosition0() {
+	public void testInsertOnPosition0() {
 		TicTacToe ttt = new TicTacToe();
-		ttt.insertX(0);
+		ttt.insert(0);
 		assertEquals(1,ttt.getValueAtPos(0));
 	}
 
 	@Test
 	public void testInsertOnTaken() {
 		TicTacToe ttt = new TicTacToe();
-		ttt.insertO(2);
-		ttt.insertX(2);
-		assertEquals(2, ttt.getValueAtPos(2));
+		ttt.insert(2);
+		ttt.insert(2);
+		assertEquals(1, ttt.getValueAtPos(2));
 	}
 
 	@Test
@@ -26,7 +26,7 @@ public class TicTacToeTests {
 		TicTacToe ttt = new TicTacToe();
 		boolean exceptionCaught = false;
 		try {
-			ttt.insertX(-1);
+			ttt.insert(-1);
 		}catch(Exception e) {
 			exceptionCaught = true;
 		}
@@ -37,5 +37,22 @@ public class TicTacToeTests {
 	public void testGetValueOutOfBounds() {
 		TicTacToe ttt = new TicTacToe();
 		assertEquals(-1, ttt.getValueAtPos(12));
+	}
+	
+	@Test
+	public void testTwoInserts() {
+		TicTacToe ttt = new TicTacToe();
+		ttt.insert(0);
+		ttt.insert(1);
+		assertEquals(2, ttt.getValueAtPos(1));
+	}
+	
+	@Test
+	public void testThreeInserts() {
+		TicTacToe ttt = new TicTacToe();
+		ttt.insert(0);
+		ttt.insert(1);
+		ttt.insert(2);
+		assertEquals(1, ttt.getValueAtPos(2));
 	}
 }
